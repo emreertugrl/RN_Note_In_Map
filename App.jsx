cdd76ff5a6f886e352c20ac68254721273b3a15f
emreertugrl <emrertugrl@gmail.com>
2025-04-29 20:42:54 +0300

    UI Kitten installation

--- App.jsx
+++ App.jsx
@@ -1,10 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/router/rootNavigator';
 
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider} from '@ui-kitten/components';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
