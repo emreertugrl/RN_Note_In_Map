6862c24a1cdc42469c36763fefd1a0ed3179a92b
emreertugrl <emrertugrl@gmail.com>
2025-04-30 10:34:39 +0300

    delete defined with firebase firestore

--- src/components/notes/noteItem.jsx
+++ src/components/notes/noteItem.jsx
@@ -1,24 +1,36 @@
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {Colors} from '../../theme/colors';
-import {Edit2} from 'iconsax-react-nativejs';
+import {Edit2, Trash} from 'iconsax-react-nativejs';
 import {useNavigation} from '@react-navigation/native';
 import {ADDNOTE} from '../../utils/routes';
-
+import firestore from '@react-native-firebase/firestore';
 const NoteItem = ({item}) => {
   const navigation = useNavigation();
+  const deleteNote = id => {
+    firestore()
+      .collection('Notes')
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log('User deleted!');
+      });
+  };
   return (
     <View style={styles.container}>
       <View style={styles.infoContainer}>
         <Text style={styles.title}>{item.title}</Text>
         <Text style={styles.date}>{item.date}</Text>
         <Text style={styles.description}>{item.description}</Text>
+        <Text style={styles.time}>{item.time}</Text>
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           onPress={() => navigation.navigate(ADDNOTE, {updateNote: item})}>
-          <Edit2 size={28} color={Colors.BLACK} variant="Bold" />
+          <Edit2 size={28} color={Colors.GREEN} variant="Bold" />
         </TouchableOpacity>
-        <Text style={styles.time}>{item.time}</Text>
+        <TouchableOpacity onPress={() => deleteNote(item.id)}>
+          <Trash size={28} color={Colors.RED} variant="Bold" />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -37,8 +49,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   buttonContainer: {
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    gap: 10,
   },
   title: {
     fontSize: 18,
@@ -56,7 +69,6 @@ const styles = StyleSheet.create({
   time: {
     fontSize: 16,
     color: Colors.GRAY,
-    textAlign: 'right',
   },
 });
 
