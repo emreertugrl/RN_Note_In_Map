b9d3a35d6739a933b4ac55ada5d927cc9f944dee
emreertugrl <emrertugrl@gmail.com>
2025-04-30 19:42:39 +0300

    android sdk with react native maps

--- src/screens/maps/index.jsx
+++ src/screens/maps/index.jsx
@@ -1,20 +1,31 @@
-import {View, Text, StyleSheet} from 'react-native';
-import {screenStyle} from '../../styles/screenStyles';
+import {View, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const Maps = () => {
   return (
-    <View style={screenStyle.container}>
-      <Text>Maps</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 40.97697,
+          longitude: 29.0419004,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#2c3e50',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
