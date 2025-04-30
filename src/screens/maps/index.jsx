218aa474c3feb600e51735b36695d4dd9eb3aa17
emreertugrl <emrertugrl@gmail.com>
2025-04-30 19:52:36 +0300

    static marker render to maps

--- src/screens/maps/index.jsx
+++ src/screens/maps/index.jsx
@@ -1,7 +1,19 @@
 import {View, StyleSheet} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const Maps = () => {
+  const markers = [
+    {
+      title: 'Not-1',
+      description: 'Not-1 açıklaması',
+      coordinate: {
+        latitude: 40.97697,
+        longitude: 29.0419004,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      },
+    },
+  ];
   return (
     <View style={styles.container}>
       <MapView
@@ -12,7 +24,16 @@ const Maps = () => {
           longitude: 29.0419004,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
-        }}></MapView>
+        }}>
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
     </View>
   );
 };
